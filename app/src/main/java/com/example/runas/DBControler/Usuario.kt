a59092632643dcda744f63a26ca3d6a97eec9233
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-18 17:44:10 +0100

    Usuario.kt CREATE

---
+++ app/src/main/java/com/example/runas/DBControler/Usuario.kt
@@ -0,0 +1,23 @@
+package com.example.runas.DBControler
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
+
+@Entity(tableName = "usuarios")
+data class Usuario (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "usuario")
+    val usuario: String,
+
+    @ColumnInfo(name = "contrasenya")
+    val contrasenya: String,
+){// Función redefinida para mostrar los datos correctamente
+    override fun toString(): String {
+
+        return "Usuario: "+usuario +" Contrasenya: "+contrasenya
+    }
+}
