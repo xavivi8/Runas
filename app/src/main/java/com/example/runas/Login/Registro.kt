6b9adfae62553b4a4b8b3eef0b49b3c550f43a93
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-21 11:37:12 +0100

    registro.kt CHANGES

--- app/src/main/java/com/example/runas/Login/Registro.kt
+++ app/src/main/java/com/example/runas/Login/Registro.kt
@@ -2,11 +2,50 @@ package com.example.runas.Login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.runas.DBControler.UsuarioDatabase
 import com.example.runas.R
 
 class Registro : AppCompatActivity() {
+    lateinit var database: UsuarioDatabase
+    lateinit var guardar: Button
+    lateinit var cancelar: Button
+    lateinit var user: EditText
+    lateinit var pass: EditText
+    lateinit var passAgain: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        guardar = findViewById(R.id.btnGuardar)
+        cancelar = findViewById(R.id.btnCancelar)
+        user = findViewById(R.id.editTextUser)
+        pass = findViewById(R.id.editTextPass)
+        passAgain = findViewById(R.id.editTextPassAgain)
+
+        guardar.setOnClickListener {
+
+            if (comprobarContra(pass.text.toString(), passAgain.text.toString())) {
+                showToast("Las contraseñas coinciden.")
+                finish()
+            } else {
+                showToast("Las contraseñas no coinciden.")
+            }
+        }
+
+        cancelar.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun comprobarContra(pass:String, passAgain:String):Boolean{
+        return pass == passAgain
+    }
+
+    private fun showToast(text:String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
