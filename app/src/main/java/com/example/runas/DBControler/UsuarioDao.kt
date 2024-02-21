a771aeab04a20771b93596ee53ea0e16f6ca595b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-21 12:20:19 +0100

    UsuarioDao CHANGES insertUsuarioSafe ADD

--- app/src/main/java/com/example/runas/DBControler/UsuarioDao.kt
+++ app/src/main/java/com/example/runas/DBControler/UsuarioDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 
 @Dao
 interface UsuarioDao {
@@ -11,6 +12,21 @@ interface UsuarioDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertarUsuario(usuarios: Usuario)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUsuario(usuario: Usuario): Long
+
     @Query("SELECT id FROM usuarios WHERE usuario = :usuario AND contrasenya = :contrasenya;")
     suspend fun checkUsuario(usuario: String, contrasenya: String): Long?
+
+    // Método para insertar un nuevo usuario y devolver true si la inserción fue exitosa, false en caso contrario
+    @Transaction
+    fun insertUsuarioSafe(usuario: Usuario): Boolean {
+        return try {
+            insertUsuario(usuario)
+            true // La inserción fue exitosa
+        } catch (e: Exception) {
+            false // La inserción falló
+        }
+    }
+
 }
