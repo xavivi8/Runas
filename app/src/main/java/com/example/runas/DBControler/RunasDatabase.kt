9dfab20906c3f98948307068a879b1f2c3c22abc
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-10 13:11:17 +0100

    RunasDatabase.kt CREATE

---
+++ app/src/main/java/com/example/runas/DBControler/RunasDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.runas.DBControler
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Runas::class], version = 1)
+abstract class RunasDatabase:  RoomDatabase() {
+    abstract  fun runasDao(): RunasDao
+
+    companion object{
+        @Volatile
+        private  var instance: RunasDatabase? = null
+        private val DBNAME = "runas.db"
+        private val LOCK = Any()
+
+        operator  fun invoke(contexto: Context) = instance ?: synchronized((LOCK)){
+            instance ?: buildDatabase(contexto).also { instance = it }
+        }
+
+        private fun buildDatabase(contexto: Context) = Room.databaseBuilder(
+            contexto, RunasDatabase::class.java, DBNAME
+        ).fallbackToDestructiveMigration().build()
+    }
+}
