747533039efc62e1d4b20ef994cd2554c1cb5675
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-10 12:39:43 +0100

    Runas.kt CREATE

---
+++ app/src/main/java/com/example/runas/DBControler/Runas.kt
@@ -0,0 +1,35 @@
+package com.example.runas.DBControler
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "runas")
+class Runas (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id_runa: Long = 0,
+
+    @ColumnInfo(name = "usuario")
+    val id_usuario: Long,
+
+    @ColumnInfo(name = "runaprincipal")
+    val runaPrincipal: Long,
+
+    @ColumnInfo(name = "subRunasPrincipal")
+    val subRunasPrincipal: String,
+
+    @ColumnInfo(name = "runasecundaria")
+    val runaSecundaria: Long,
+
+    @ColumnInfo(name = "subRunasSevundaria")
+    val subRunasSevundaria: String,
+
+    @ColumnInfo(name = "ventajasAdicionales")
+    val ventajasAdicionales: String,
+){
+    override fun toString(): String {
+
+        return "ID Runa: "+id_runa +" Id Usuario: "+id_usuario+" Sus runas"
+    }
+}
