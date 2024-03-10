1619dedf7e9620385259d09698688d48bd18b193
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-10 18:50:50 +0100

    InsertarRunas.kt CREATE

---
+++ app/src/main/java/com/example/runas/Runas/InsertarRunas.kt
@@ -0,0 +1,13 @@
+package com.example.runas.Runas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runas.R
+
+class InsertarRunas : AppCompatActivity() {
+    val va: Long = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insertar_runas)
+    }
+}
