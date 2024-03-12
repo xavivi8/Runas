9e140d5cfa7894949759442964a4963323ace298
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-12 10:37:41 +0100

    EditViewRuna.kt CREATE

---
+++ app/src/main/java/com/example/runas/Runas/EditViewRuna.kt
@@ -0,0 +1,12 @@
+package com.example.runas.Runas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runas.R
+
+class EditViewRuna : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_view_runa)
+    }
+}
