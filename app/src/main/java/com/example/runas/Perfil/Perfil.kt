1e4ba921808d5aa8a846d87156f8f55bbc0bbaf6
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-13 10:20:24 +0100

    Perfil.kt CREATE

---
+++ app/src/main/java/com/example/runas/Perfil/Perfil.kt
@@ -0,0 +1,12 @@
+package com.example.runas.Perfil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runas.R
+
+class Perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
