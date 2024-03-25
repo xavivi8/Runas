beeb6684dd5bf20d2938582c4be6a95113f0333e
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-25 10:44:35 +0100

    Dependencia para acceder a la api ADD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Dependencia para acceder a la api
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.8.9")
+
     // ROOM y SQLITE
 
     val room_version = "2.5.0"
