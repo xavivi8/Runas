fe1dc970bf1af0c36005ca05f0b8d7ebf6ef0478
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-02 18:26:50 +0200

    UsuarioDatabase.kt UPDATE version

--- app/src/main/java/com/example/runas/DBControler/UsuarioDatabase.kt
+++ app/src/main/java/com/example/runas/DBControler/UsuarioDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.Room
 
-@Database(entities = [Usuario::class], version = 3)
+@Database(entities = [Usuario::class], version = 4)
 abstract class UsuarioDatabase : RoomDatabase() {
 
     abstract fun usuarioDao(): UsuarioDao
